6d85260fa1b54d7b2eb836e7ccf04c9232839270
codeywgithub <codey.atkinson@gmail.com>
2020-05-11 17:28:54 +1000

    FirstLine my name is

---
+++ PartD/src/mainClass.java
@@ -0,0 +1,14 @@
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi my name is Codey");
+		
+		
+		
+		
+	}
+
+}
