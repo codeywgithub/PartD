6d6ef56f4d2c835791037df8954d7a96d50529ec
codeywgithub <codey.atkinson@gmail.com>
2020-05-12 09:13:28 +1000

    Add best day of the week

--- PartD/src/mainClass.java
+++ PartD/src/mainClass.java
@@ -11,7 +11,7 @@ public class mainClass {
 		System.out.println("My favourite mode of transportation is the car");
 		
 		
-		
+		System.out.println("The best day of the week is Saturday");
 		
 	}
 
